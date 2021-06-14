c0466342d1399cf31798c57fb7f0bbc591d0f6ea
yunseokun <yunseokun789@gmail.com>
2021-06-14 19:57:40 +0900

    자바스크립트 1일차

---
+++ index.js
@@ -0,0 +1,32 @@
+//let arr = [1,2,3,4,5];
+//console.log(arr);
+
+
+//let person = {
+//    name: "윤석훈",
+//    age: 17
+//};
+//
+//console.log(person);
+
+//let a = 1;
+//let b = "1";
+//
+//if(a == b) {
+//    console.log(`a와 b는 같다`); //값만 검사
+//}
+//if(a === b) {
+//    console.log(`a와 b는 값과 타입이 같다`);
+//}
+
+//let arr = [1,2,3,4,5];
+//
+//for(let i = 0; i < arr.length; i++) {
+//    console.log(arr[i]);
+//}
+
+let arr = [1,2,3,4,5];
+
+for(let i in arr) {
+    console.log(arr[i]);
+}
